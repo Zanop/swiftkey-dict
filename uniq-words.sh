91e5ddd72f307c0c5ba74f9cbe91ec567e01bc73
Vladimir Smolensky <v.smolensky@ucdn.com>
2017-12-01 20:49:01 +0200

    initial commit

---
+++ uniq-words.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $# < 1 ]]; then
+  echo "Usage: $0 text_file"
+  exit 1
+fi
+if [[ ! -f $1 ]]; then
+  echo "Argument 1 is not a file"
+  exit 2
+fi
+
+grep -o -E '\w+' "$1" | grep -E '^[а-зА-З]+$' | sort | uniq > $(basename $1)_uniq.txt
+cat *_uniq.txt | sort | uniq > bgwords.txt
